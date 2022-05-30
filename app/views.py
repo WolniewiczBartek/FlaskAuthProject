d07e8f2c1686182c5e22e6cf0e9624b91501ce6b
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-05-30 22:36:08 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
+from .models import User, Order
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def home():
-    return render_template('home.html')
 
+@views.route('/')
+@login_required
+def home():
+    return render_template('home.html', user=current_user)
+
+@views.route('/users')
+@login_required
+def users():
+    return render_template('users.html', user=current_user, users=User.query.all())
