e96f89f8685f4d8a0ca0c3f1646470c261fe631f
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-06-01 17:32:25 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,16 +1,35 @@
-from flask import Blueprint, render_template
-from .models import User, Order
+from flask import Blueprint, render_template, session
+from .models import User, Order, Role, Product
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
-@login_required
 def home():
-    return render_template('home.html', user=current_user)
+    return render_template('home.html', user=current_user, products=Product.query.filter_by(visible=True).all())
 
-@views.route('/users')
-@login_required
-def users():
-    return render_template('users.html', user=current_user, users=User.query.all())
+@views.route('/product/<id>')
+def product(id):
+    return render_template('product.html', user=current_user, product=Product.query.filter_by(id=id).first())
+
+@views.route('/add_to_cart/<prod_id>', methods=['POST'])
+def add_to_cart(prod_id):
+    
+    if 'cart' not in session:
+        session['cart'] = {}
+    if str(prod_id) in session.get('cart').keys():
+        session['cart'][str(prod_id)] += 1
+    else:
+        session['cart'][str(prod_id)] = 1
+    return render_template('home.html', user=current_user)
+        
+@views.route('/cart')
+def cart():
+    if 'cart' in session:
+        cart = session['cart']
+    else:
+        cart = None
+    return render_template('cart.html', user=current_user, cart=cart)
+        
+        
